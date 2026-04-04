b0566cc355fe384cd965da3f7a207929a3fd14fe
Damien Haynes <dhaynes@exegy.com>
2026-04-04 16:18:34 +1100

    Fixed null reference exception for sync of liked lists
    Seems like the model has changed on the API since this was implemented. It only affects the "min" API i.e. on min they add a id property but on the full they have a ids e.g.
    
    {
        "liked_at": "2019-12-30T01:30:48.000Z",
        "type": "list",
        "list": {
          "name": "Trakt: Popular Movies",
          "description": "The Trakt top 250 movies, based on popularity. List updated daily.",
          "privacy": "public",
          "share_link": "https://trakt.tv/lists/4834049",
          "type": "personal",
          "display_numbers": true,
          "allow_comments": true,
          "sort_by": "rank",
          "sort_how": "asc",
          "created_at": "2018-05-13T16:33:23.000Z",
          "updated_at": "2026-03-31T14:02:40.000Z",
          "item_count": 251,
          "comment_count": 0,
          "likes": 402,
          "ids": {
            "trakt": 4834049,
            "slug": "trakt-popular-movies"
          },
          "user": {
            "username": "justin",
            "private": false,
            "deleted": false,
            "name": "Justin",
            "vip": true,
            "vip_ep": true,
            "director": true,
            "ids": {
              "slug": "justin"
            },
            "joined_at": "2010-09-25T17:49:25.000Z",
            "location": "United States",
            "about": "Do the truffle shuffle!!",
            "gender": "male",
            "age": 44,
            "images": {
              "avatar": {
                "full": "https://walter-r2.trakt.tv/images/admins/000/000/001/avatars/large/2e6df69058.jpg"
              }
            }
          }
        }
      }
    
    vs
    
      {
        "liked_at": "2015-09-18T01:23:14.000Z",
        "type": "list",
        "list": {
          "id": 1402475
        }
      },

--- TraktAPI/DataStructures/TraktLike.cs
+++ TraktAPI/DataStructures/TraktLike.cs
@@ -3,51 +3,51 @@ using System.Runtime.Serialization;
 
 namespace TraktAPI.DataStructures
 {
-    [DataContract]
-    public class TraktLike : IEquatable<TraktLike>
+  [DataContract]
+  public class TraktLike : IEquatable<TraktLike>
+  {
+    [DataMember( Name = "liked_at" )]
+    public string LikedAt { get; set; }
+
+    [DataMember( Name = "type" )]
+    public string Type { get; set; }
+
+    [DataMember( Name = "list" )]
+    public TraktListDetail List { get; set; }
+
+    [DataMember( Name = "comment" )]
+    public TraktComment Comment { get; set; }
+
+    #region IEquatable
+    public bool Equals( TraktLike other )
     {
-        [DataMember(Name = "liked_at")]
-        public string LikedAt { get; set; }
+      if ( other == null || ( other.Comment == null && other.Type == "comment" ) || ( other.List == null && other.Type == "list" ) )
+        return false;
 
-        [DataMember(Name = "type")]
-        public string Type { get; set; }
+      if ( this.Type == "list" )
+      {
+        if ( this.List.Id == null || other.List.Id == null )
+          return false;
 
-        [DataMember(Name = "list")]
-        public TraktListDetail List { get; set; }
-
-        [DataMember(Name = "comment")]
-        public TraktComment Comment { get; set; }
-
-        #region IEquatable
-        public bool Equals(TraktLike other)
-        {
-            if (other == null || (other.Comment == null && other.Type == "comment") || (other.List == null && other.Type == "list"))
-                return false;
-
-            if (this.Type == "list")
-            {
-                if (this.List.Ids == null || other.List.Ids == null)
-                    return false;
-
-                return (this.Type == other.Type && this.List.Ids.Trakt == other.List.Ids.Trakt);
-            }
-            else
-            {
-                return (this.Type == other.Type && this.Comment.Id == other.Comment.Id);
-            }
-        }
-
-        public override int GetHashCode()
-        {
-            if (this.Type == "list")
-            {
-                return (this.List.Ids.Trakt.ToString() + "_" + this.Type).GetHashCode();
-            }
-            else
-            {
-                return (this.Comment.Id.ToString() + "_" + this.Type).GetHashCode();
-            }
-        }
-        #endregion
+        return ( this.Type == other.Type && this.List.Id == other.List.Id );
+      }
+      else
+      {
+        return ( this.Type == other.Type && this.Comment.Id == other.Comment.Id );
+      }
     }
+
+    public override int GetHashCode()
+    {
+      if ( this.Type == "list" )
+      {
+        return ( this.List.Id.ToString() + "_" + this.Type ).GetHashCode();
+      }
+      else
+      {
+        return ( this.Comment.Id.ToString() + "_" + this.Type ).GetHashCode();
+      }
+    }
+    #endregion
+  }
 }
